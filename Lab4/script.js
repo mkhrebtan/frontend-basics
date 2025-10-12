784783a559047157aedd316b36317a41b9035cbd
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-10-12 21:17:43 +0300

    complete Lab4 tasks

---
+++ Lab4/script.js
@@ -0,0 +1,63 @@
+function onHobbyMusicClick() {
+    const hobby = document.getElementById('music');
+    hobby.style.backgroundColor = getRandomColor();
+    hobby.style.color = getRandomColor();
+}
+
+function onFavouriteMoviesLabelClick() {
+    const label = document.querySelector('.favourite-movies-label');
+    label.style.backgroundColor = getRandomColor();
+    label.style.color = getRandomColor();
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+let imageCount = 1;
+
+function addImage() {
+    const img = document.createElement('img');
+    const existingImg = document.getElementById('city-image-1');
+    img.src = existingImg.src;
+    img.alt = existingImg.alt;
+    img.id = `city-image-${++imageCount}`;
+
+    const cityLink = document.getElementById('city-link-1');
+    const cityLinkClone = cityLink.cloneNode(false);
+    cityLinkClone.id = `city-link-${imageCount}`;
+    cityLinkClone.appendChild(img);
+
+    const buttonsDiv = document.querySelector('.image-buttons');
+    buttonsDiv.insertAdjacentElement('beforebegin', cityLinkClone);
+}
+
+function enlargeImage() {
+    const lastImage = document.getElementById(`city-image-${imageCount}`);
+    if (lastImage) {
+        lastImage.style.width = (lastImage.width * 1.2) + 'px';
+        lastImage.style.height = (lastImage.height * 1.2) + 'px';
+    }
+}
+
+function reduceImage() {
+    const lastImage = document.getElementById(`city-image-${imageCount}`);
+    if (lastImage) {
+        lastImage.style.width = (lastImage.width * 0.8) + 'px';
+        lastImage.style.height = (lastImage.height * 0.8) + 'px';
+    }
+}
+
+function removeImage() {
+    if (imageCount > 1) {
+        const lastImageLink = document.getElementById(`city-link-${imageCount}`);
+        lastImageLink.remove();
+        imageCount--;
+    }
+}
